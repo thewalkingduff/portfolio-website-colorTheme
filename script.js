2efef7a0c05a7eb003ba8ccab7c658da5c9064a5
thewalkingduff <62387646+thewalkingduff@users.noreply.github.com>
2021-01-11 00:26:56 -0500

    moved social icons to nav

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-console.log('Its working')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-let theme = localStorage.getItem('theme')
-
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
-}
-
-let themeDots = document.getElementsByClassName('theme-dot')
-
-
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
-}
-
-function setTheme(mode){
-	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
-	}
-
-	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
-	}
-
-	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
-	}
-
-	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
-	}
-
-	localStorage.setItem('theme', mode)
-}
+ReactDOM.render( <
+	React.StrictMode >
+	<
+	App / >
+	<
+	/React.StrictMode>,
+	document.getElementById('root')
+);
